2faaed19f6d7544bd88207cabf39983ff07671ee
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-03-23 09:10:42 -0400

    fixed errors

--- src/actions/storage.js
+++ src/actions/storage.js
@@ -1,23 +1,38 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const getMovies = async () => {
-    try {
-        const jsonValue = await AsyncStorage.getItem('movies')
-        console.log(jsonValue)
-        return jsonValue != null ? JSON.parse(jsonValue) : null;
-    } catch (error) {
-        console.log(error)
+  try {
+    const storedMovies = await AsyncStorage.getItem("movies");
+    if (storedMovies !== null) {
+      return await JSON.parse(storedMovies);
+      console.log("Movies retrieved successfully.");
     }
+  } catch (error) {
+    console.error("Error retrieving names:", error);
+  }
+};
 
-}
-
-export const saveMovie = async (movieName) => {
-    try {
-        const jsonValue = await getMovies();
-        const appendedMovies = [...jsonValue, movieName]
-        await AsyncStorage.setItem('movies', JSON.stringify(appendedMovies))
-
-    } catch (error) {
-        console.log(error)
+export const saveMovie = async (movie) => {
+  try {
+    const storedMovies = await AsyncStorage.getItem("movies");
+    if (storedMovies !== null) {
+      const parsedMovies = JSON.parse(storedMovies);
+      parsedMovies.push(movie);
+      await AsyncStorage.setItem("movies", JSON.stringify(parsedMovies));
+      console.log("Movie saved successfully.");
+    } else {
+      await AsyncStorage.setItem("movies", JSON.stringify([movie]));
+      console.log("Movie saved successfully.");
     }
-}
+  } catch (error) {
+    console.error("Error saving movie:", error);
+  }
+};
+
+export const clearMovies = async () => {
+  try {
+    await AsyncStorage.removeItem("movies");
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/app/(home)/movieadd.js
+++ src/app/(home)/movieadd.js
@@ -1,93 +1,115 @@
-import { Redirect, router } from 'expo-router'
-import { useState } from 'react'
-import { View, Text, TextInput, Dimensions, FlatList, TouchableOpacity } from 'react-native'
-import SelectableMovieItem from '../../components/SelectableMovieItem'
-import { saveMovie } from '../../actions/storage'
-
+import { Redirect, router } from "expo-router";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Dimensions,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import SelectableMovieItem from "../../components/SelectableMovieItem";
+import { saveMovie } from "../../actions/storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const MovieAdd = () => {
-    const { width, height } = Dimensions.get('window')
-    const [query, setQuery] = useState('')
-    const [movies, setMovies] = useState([])
+  const { width, height } = Dimensions.get("window");
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
 
-    const [selectedMovieName, setSelectedMovieName] = useState('')
-    const [selectedMovieKey, setSelectedMovieKey] = useState('')
+  const [selectedMovieName, setSelectedMovieName] = useState("");
+  const [selectedMovieKey, setSelectedMovieKey] = useState("");
 
-    const onHandleSubmit = async () => {
-        const searchQuery = query.replace(/ /g, "%20");
-        const options = {
-            method: "GET",
-            headers: {
-              accept: "application/json",
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNGFmY2YyZTJjMWQyMzBkYWM5NzIzNGU5MWUxMjQ5MyIsInN1YiI6IjY1ZjhlMjFhMzNhMzc2MDE4NDM1YmRmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R-_0PyJQAm80I6PKvEfGXnwYhBPjyKIZSSW6yJnPCfE",
-            },
-          };
+  const onHandleSubmit = async () => {
+    const searchQuery = query.replace(/ /g, "%20");
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNGFmY2YyZTJjMWQyMzBkYWM5NzIzNGU5MWUxMjQ5MyIsInN1YiI6IjY1ZjhlMjFhMzNhMzc2MDE4NDM1YmRmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R-_0PyJQAm80I6PKvEfGXnwYhBPjyKIZSSW6yJnPCfE",
+      },
+    };
 
-        const movieDataRaw = await fetch(`https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&primary_release_year=545611&page=1`,
-        options)
+    const movieDataRaw = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&primary_release_year=545611&page=1`,
+      options
+    );
 
-        const movieData = await movieDataRaw.json()
-        console.log(movieData.results)
-        setMovies(movieData.results)
-    }
+    const movieData = await movieDataRaw.json();
+    setMovies(movieData.results);
+  };
 
   return (
-    <View style={{
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'white',
-        alignItems: 'center',
-    }}>
-        <TextInput 
-        placeholder='Enter movie name...'
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "white",
+        alignItems: "center",
+      }}
+    >
+      <TextInput
+        placeholder="Enter movie name..."
         style={{
-            backgroundColor: '#F1F1F1',
-            width: width * .9,
-            height: height * .05,
-            marginTop: width * .03,
-            paddingLeft: width * .05,
-            borderRadius: 20,
-            fontWeight: 'bold',
-            fontSize: width * .04,
-            marginBottom: width * .03,
-
+          backgroundColor: "#F1F1F1",
+          width: width * 0.9,
+          height: height * 0.05,
+          marginTop: width * 0.03,
+          paddingLeft: width * 0.05,
+          borderRadius: 20,
+          fontWeight: "bold",
+          fontSize: width * 0.04,
+          marginBottom: width * 0.03,
         }}
         onChangeText={(text) => setQuery(text)}
         onSubmitEditing={onHandleSubmit}
-        />
-        <FlatList
+      />
+      <FlatList
         style={{
-            width: '100%',
+          width: "100%",
         }}
-
         contentContainerStyle={{
-            alignItems: 'center',
-        
+          alignItems: "center",
         }}
         data={movies}
-        renderItem={({item, index}) => {
-            if (item.title === selectedMovieName && index.toString() === selectedMovieKey){
-                return (<SelectableMovieItem movieName={item.title} selected={true} setSelected={() => {
-                    setSelectedMovieName('')
-                    setSelectedMovieKey('')
-                }} />)
-            } else {
-                return (<SelectableMovieItem movieName={item.title} selected={false} setSelected={() => {
-                    setSelectedMovieName(item.title)
-                    setSelectedMovieKey(index.toString())
-                }} />)
-            }
+        renderItem={({ item, index }) => {
+          if (
+            item.title === selectedMovieName &&
+            index.toString() === selectedMovieKey
+          ) {
+            return (
+              <SelectableMovieItem
+                movieName={item.title}
+                selected={true}
+                setSelected={() => {
+                  setSelectedMovieName("");
+                  setSelectedMovieKey("");
+                }}
+              />
+            );
+          } else {
+            return (
+              <SelectableMovieItem
+                movieName={item.title}
+                selected={false}
+                setSelected={() => {
+                  setSelectedMovieName(item.title);
+                  setSelectedMovieKey(index.toString());
+                }}
+              />
+            );
+          }
         }}
         keyExtractor={(item, index) => index}
-        />
+      />
 
-        <TouchableOpacity
+      <TouchableOpacity
         onPress={async () => {
-            if (selectedMovieName !== ''){
-                await saveMovie(selectedMovieName)
-                router.replace('/home')
-            }
+          if (selectedMovieName !== "") {
+            await saveMovie(selectedMovieName);
+            router.replace("/home");
+          }
         }}
         style={{
           backgroundColor: "#C7F4FC",
@@ -111,7 +133,7 @@ const MovieAdd = () => {
         </Text>
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default MovieAdd
+export default MovieAdd;
